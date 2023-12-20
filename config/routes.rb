c948588e3c8c99e505ca4ccae22d05ad9fa04f9a
Thomas Hawley <thawley22@turing.edu>
2023-12-20 14:03:12 -0700

    Add root route to players#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
+  
+  root 'players#index'
+
   resources :players do
     collection do
       get 'list'
     end
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
