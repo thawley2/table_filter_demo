33638d3df3feb86a4ffa041d86bd3d3d48ac0d6a
Thomas Hawley <thawley22@turing.edu>
2023-12-21 14:35:42 -0700

    Refactor sort_link method in PlayersHelper

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -1,17 +1,17 @@
 module PlayersHelper
-  def sort_link(column:, label:)
-    if params[:column] == column
+  def build_order_link(column:, label:)
+    if column == session.dig('player_filters', 'column')
       link_to(label, list_players_path(column: column, direction: next_direction))
     else
       link_to(label, list_players_path(column: column, direction: 'asc'))
     end
   end
 
-    def next_direction
-      params[:direction] == 'asc' ? 'desc' : 'asc'
-    end
+  def next_direction
+    session['player_filters']['direction'] == 'asc' ? 'desc' : 'asc'
+  end
 
-    def sort_indicator
-      tag.span(class: "sort sort-#{params[:direction]}")
-    end
+  def sort_indicator
+    tag.span(class: "sort sort-#{session['player_filters']['direction']}")
+  end
 end
