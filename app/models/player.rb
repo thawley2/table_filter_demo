1e8d7f331ce10214611180bc74170a8e3b320f05
Thomas Hawley <thawley22@turing.edu>
2023-12-21 15:42:42 -0700

    Refactor Player model's filter method to include team filtering

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,13 +1,15 @@
 class Player < ApplicationRecord
   belongs_to :team
 
-  FILTER_PARAMS = %i[name column direction].freeze
+  FILTER_PARAMS = %i[name column direction team_id].freeze
 
-  scope :by_name, ->(query) { where('players.name ilike ?', "%#{query}%")}
+  scope :by_name, ->(name) { where('players.name ilike ?', "%#{name}%")}
+  scope :by_team, ->(team_id) { where('players.team_id = ?', team_id) if team_id.present? }
 
   def self.filter(filters)
     Player.includes(:team)
           .by_name(filters['name'])
+          .by_team(filters['team_id'])
           .order("#{filters['column']} #{filters['direction']}")
   end
 end
